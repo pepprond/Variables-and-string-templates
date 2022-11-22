eef302a714f88c64ad45c0ba69032e7153a1d736
pepprond <rondelli95@gmail.com>
2022-11-22 11:52:51 +0100

    final commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    val number1: Int = 345
+    val number2= 123
+    val number3: Double =12.55
+    println("Hello! number1: ${number1}, number2: ${number2}, number3: ${number3}")
+
+}
